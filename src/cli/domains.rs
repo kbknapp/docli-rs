71c4e4dc07216a015a37c13e7eec3ad4ad9659e2
Kevin K <kbknapp@gmail.com>
2015-05-15 13:49:26 -0400

    wip(domains): stub functionality for domains

--- src/cli/domains.rs
+++ src/cli/domains.rs
@@ -6,9 +6,24 @@ use cli::errors::{CliResult, CliError};
 
 pub fn run(m: &ArgMatches, cfg: &Config) -> CliResult {
     match m.subcommand() {
-        ("create", Some(m))   => Ok(()),
-        ("show", Some(m))     => Ok(()),
-        ("delete", Some(m))   => Ok(()),
-        _                     => Err(CliError::NoCommand)
+        ("create", Some(m))      => {
+            // unwrap is safe because args are required
+            let name = m.value_of("name").unwrap();
+            // TODO: Validate IP
+            let ip   = m.value_of("ip").unwrap();
+            println!("Creating domain '{}' with IP: {}", name, ip);
+            Ok(())
+        },
+        ("show-domain", Some(m)) => {
+            let name = m.value_of("name").unwrap();
+            println!("Showing domain: {}", name);
+            Ok(())
+        },
+        ("delete", Some(m))      => {
+            let name = m.value_of("name").unwrap();
+            println!("Deleting domain: {}", name);
+            Ok(())
+        },
+        _                        => Err(CliError::NoCommand)
     }
 }
