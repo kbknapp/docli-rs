8e3dc256acf576beedce9da994e9b532d5568bc9
Kevin K <kbknapp@gmail.com>
2015-05-17 21:39:52 -0400

    wip(account): begin real implementation

--- src/cli/account.rs
+++ src/cli/account.rs
@@ -1,6 +1,7 @@
 use clap::ArgMatches;
 
 use config::Config;
+use libdo::DoManager;
 
 pub fn run(m: &ArgMatches, cfg: &Config) {
     match m.subcommand() {
@@ -9,7 +10,13 @@ pub fn run(m: &ArgMatches, cfg: &Config) {
             println!("Retrieving action id: {}", m.value_of("id").unwrap());
         },
         ("", None)               => {
-            println!("Showing account info")
+            if cfg.debug { println!("Displaying account with token:\n\t{}", &cfg.auth[..])}
+            let domgr = DoManager::with_token(&cfg.auth[..]);
+            let acct = domgr.account.request;
+            if cfg.debug { println!("Sending request: {:?}", acct); }
+            if !cfg.no_send {
+                println!("{}", domgr.account.retrieve());
+            }
         },
         _ => unreachable!()
     }
